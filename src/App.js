db41e0073da4ebd1b56a4f3b0d98036604e860a9
MatheusEli <matheuselisouzaferreira@hotmail.com>
2023-12-15 01:36:58 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import './sass/main.scss';
+import HomePage from './pages/Home';
+import ContactPage from './pages/Contact';
+import LocationsPage from './pages/Locations';
+import AboutPage from './pages/About';
+import GraphicDesignPage from './pages/GraphicDesign';
+import AppDesignPage from './pages/AppDesign';
+import WebDesign from './pages/WebDesign';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className='container'>
+    <Routes>
+      <Route exact path="/" element={<HomePage />}/>
+      <Route exact path="/web-design" element={<WebDesign />}/>
+      <Route exact path="/app-design" element={<AppDesignPage />}/>
+      <Route exact path="/graphic-design" element={<GraphicDesignPage />}/>
+      <Route exact path="/about" element={<AboutPage />}/>
+      <Route exact path="/locations" element={<LocationsPage />}/>
+      <Route exact path="/contact" element={<ContactPage />}/>
+    </Routes>
+  </div>
   );
 }
 
